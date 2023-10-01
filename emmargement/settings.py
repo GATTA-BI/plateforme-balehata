c2ab2a9e54f8af8dc24048d4e27a56b12293d334
GATTA BI KOERY FRANCK <89083289+GATTA-BI@users.noreply.github.com>
2023-10-01 18:03:48 +0000

    Update settings.py

--- emmargement/settings.py
+++ emmargement/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'emmargement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'balehata$mysqldb',
+        'USER': 'balehata',
+        'PASSWORD' : '0193uvci',
+        'HOST' : 'balehata.mysql.pythonanywhere-services.com',
+        'PORT' : '3306',
     }
 }
 
