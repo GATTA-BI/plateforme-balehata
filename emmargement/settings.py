75efacb5504a3f3503d83b6984a7d8ec73778e44
GATTA BI KOERY FRANCK <bi.gatta@uvci.edu.ci>
2023-10-02 02:43:28 +0000

    hello word

--- emmargement/settings.py
+++ emmargement/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['templates'],
-        'APP_DIRS': True,
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -79,10 +79,22 @@ WSGI_APPLICATION = 'emmargement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bd_balehata',
+        'HOST' : '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': 3306
     }
 }
 
